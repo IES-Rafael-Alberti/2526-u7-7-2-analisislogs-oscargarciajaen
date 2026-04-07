16dccd42d3d22866eb1adaf50b0e9ce9bb9bd0e7
oscargarcia <ogarjae565@g.educaand.es>
2026-04-07 19:03:20 +0200

    Implementacion de la funcion solicitarFechaTerminal

---
+++ src/main/kotlin/Dominio/ComparadorFechas.kt
@@ -0,0 +1,11 @@
+package org.iesra.Dominio
+
+class ComparadorFechas {
+
+    companion object{
+        fun comparar(fechaInicial: String, fechaTerminal: String){
+            val dia = fechaInicial.split( " ")[0]
+
+        }
+    }
+}
--- src/main/kotlin/Presentacion/ConsoleInput.kt
+++ src/main/kotlin/Presentacion/ConsoleInput.kt
@@ -2,15 +2,61 @@ package org.iesra.Presentacion
 
 class ConsoleInput {
 
-    fun solicitarFechaInicial(){
-        // Todo solicitar fecha inicial
+    companion object {
+
+        fun solicitarFechaInicial(): String {
+            val regex = """^\\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01])\\s([01]\\d|2[0-3]):[0-5]\\d:[0-5]\\d$""".toRegex()
+
+            var fechaInicial: String? = null
+
+            do {
+                println("Introduce la fecha inicial en formato 2025-01-15 10:30:15 ")
+                val entrada = readLine()
+                if (entrada != null && entrada.matches(regex)){
+                    fechaInicial = entrada
+                }
+            } while (fechaInicial == null)
+            return fechaInicial
+        }
+
+        fun solicitarFechaTerminal(): String{
+            val regex = """^\\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01])\\s([01]\\d|2[0-3]):[0-5]\\d:[0-5]\\d$""".toRegex()
+
+            var fechaTerminal: String? = null
+
+            do {
+                println("Introduce la fecha inicial en formato 2025-01-15 10:30:15 ")
+                val entrada = readLine()
+                if (entrada != null && entrada.matches(regex)){
+                    if (){
+                        fechaTerminal = entrada
+                    }
+
+                }
+            } while (fechaTerminal == null)
+            return fechaTerminal
+        }
+
+        fun mostrarMenú(): String {
+
+            val entradasPosibles = listOf<String>("1", "2", "3", "4")
+            var decision: String? = null
+
+            do {
+                println("===== Procesador de Logs =====\n")
+                println("Elija una opción: \n")
+                println("1. Contar número de Logs")
+                println("2. Filtrar por fecha")
+                println("3. Generar análisis")
+                println("4. Salir")
+                val entrada = readLine()
+                if (entrada in entradasPosibles){
+                    decision = entrada
+                }
+            } while (decision == null)
+            return decision
+        }
+
     }
 
-    fun solicitarFechaTerminal(){
-        // Todo solicitar fecha terminarl y validarla
-    }
-
-    fun mostrarMenú(){
-        // Todo mostrar el menu de opciones
-    }
 }
